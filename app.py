fcf3c71ce1aeaea182113228e0dbf4cf175c9533
ensured <74529637+ensured@users.noreply.github.com>
2021-01-30 20:37:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,29 +9,15 @@ print(
     + "\n"
 )
 
-print("type 'backup' to backup contacts.vcf")
-
 
 def add_contact():
     while True:
         new_contact_num = str(input("Enter phone number: "))
-        if new_contact_num.lower() == "backup":
-            backup()
-        else:
-            break
         if len(new_contact_num) != 10:
             print("Phone number not 10 digits")
             continue
         new_contact_fn = input("Enter first name: ")
-        if new_contact_num.lower() == "backup":
-            backup()
-        else:
-            break
         new_contact_ln = input("Enter first name: ")
-        if new_contact_num.lower() == "backup":
-            backup()
-        else:
-            break
 
         # Formatting phone number to add '_'
         temp = "-".join(
@@ -39,15 +25,12 @@ def add_contact():
         )
         new_num = temp + new_contact_num[-1]
 
-        data = f"""
-BEGIN:VCARD
+        data = f"""BEGIN:VCARD
 VERSION:2.1
 N:{new_contact_ln};{new_contact_fn};;;
 FN:{new_contact_fn} {new_contact_ln}
 TEL;CELL:{new_num}
-END:VCARD
-    """
-
+END:VCARD"""
         try:
             with open("contacts.vcf", "a+") as f:
                 f.write(data)
@@ -55,6 +38,7 @@ END:VCARD
         except:
             print("Couldn't add contact.")
             break
+        backup()
 
 
 def backup():
@@ -64,9 +48,9 @@ def backup():
         src = os.path.realpath("contacts.vcf")
 
         # seperate the path from the filter
-        head, tail = os.path.split(src)
-        print("path:" + head)
-        print("file:" + tail)
+        # head, tail = os.path.split(src)
+        # print("path:" + head)
+        # print("file:" + tail)
 
         # let's make a backup copy by appending "bak" to the name
         dst = src + ".bak"
@@ -75,7 +59,7 @@ def backup():
 
         # copy over the permissions,modification
         shutil.copystat(src, dst)
-        print("backed up files to", head)
+        print("backed up files to", src)
         add_contact()
 
 
